0af01bc2a31a23feddc568b457da9db2beaed1f0
ldatareserve <lnowlin@mail.com>
2023-06-27 23:10:20 -0400

    Week 1 of IP Python class

---
+++ week1.py
@@ -0,0 +1,42 @@
+#Lab 1 Strings. below are a bunch of str that we have written within our first python file
+#this is an example of a normal str
+print('First str was seen on 16 April 2023. Estoy feliz a empezar mi camino de Python')
+print('Me encanto ser un hombre de IT')
+print('Prefiero mujeres felices')
+print('En la semana pasada conoci alguno gente de Asia')
+print('Quiero una familia grande')
+
+#this is an example of how to put quotes into str
+print('\'Bruh!\' Allen Iverson is the best basketball player in the world')
+print('The best Brazilian movie of all time is ''\'City of God\'.')
+#this is an example of how to put a new line
+print('New\nline')
+print('Bron is worse than Curry')
+
+#Lab 2 Variables #first variables in class
+
+my_str = 'MJ won all his NBA championship with the team that drafted him.'
+print(my_str)
+
+my_str_another = 'Allen Iverson may not have won the NBA Finals, but he was still great'
+print(my_str_another)
+
+blue = 'This is what the sky looks like.'
+print(blue)
+
+blue_green_orange_purple = 'a lot of colors'
+print(blue_green_orange_purple)
+
+#Lab 3 Variables
+
+#this is an example of how to use variable to deploy massive changes to a lot of code
+
+character_name = 'Anis Bryant'
+character_age = "22"
+character_job = 'CEO of Microsoft'
+character_country = 'Japan'
+
+print('There was a man named ' + character_name + ',')
+print('he is ' + character_age + ' years old.')
+print('He works as the '+ character_job + '.')
+print('He got married to a joyful woman from ' + character_country + '.')
