8f2d6c7fea78ec20b440bc3b5253a840ba472906
ldatareserve <lnowlin@mail.com>
2023-06-28 14:43:40 -0400

    Week 2 of IP Python class

---
+++ week2.py
@@ -0,0 +1,86 @@
+# 20 lines of str
+print('Hola')
+print('We never got to see the last \"Avengers\" movie')
+print('Zimbabwe can get rainy this time of year')
+print('You can use backslashes for contractions, can\'t you?')
+print('Awesome, I was looking for a single quote using workaround')
+print('Or is it more common to type strings and comments without an apostrophe if you use single quotes?')
+print('For instance ex: I cant seem to find the bug. or Wouldnt it be better to put this in a function?')
+print('I\'m thinking either cyber security engineer or something with AWS')
+print('Learning about this is engaging and imparts worthwhile skill')
+print('The key to this class seems to be the application of its use outside it')
+print('What\'s next loops or functions?')
+print('Will that cover try-except(else or finally)?')
+print('Also, I enjoy learning new keyboard and VSC shortcuts, \nbut I don\'t know them all, are you willing to teach those(at least the ones you use most for work)?')
+print('I pray that you are blessed and favored because you help others')
+print('Color or colour?')
+print('A guy from England told me you guys like vinegar so much because most have bitter dispositions')
+print('He said tea and crumpets are basically a light snack(finger foods)')
+print('Have you been told you that you sound like KSI?')
+print('ksIP lol')
+print('Allow it fam, just taking the piss')
+
+# 20 lines variables
+first_variable = 'test'
+print(first_variable)
+new_idea = 'weed out pookies from the good men'
+print(new_idea)
+go_team_go = 'win the race'
+print(go_team_go)
+compass = 'north, south, east, west'
+print(compass)
+prev_finals_champ = 'Golden State Warriors'
+print(prev_finals_champ)
+passed_five_finals_champs = 'Warriors, Bucks, Lakers, Raptors, Warriors'
+print(passed_five_finals_champs)
+prev_sb_champ = 'Kansas City Chiefs'
+print(prev_sb_champ)
+passed_five_sb_champs = 'Chiefs, Rams, Bucs, Chiefs, Patriots'
+print(passed_five_sb_champs)
+passed_cfb_champ = 'Georgia Bulldogs'
+print(passed_cfb_champ)
+prev_ten_cfb_champs = 'Georgia, Georgia, Alabama, LSU, Clemson, Alabama, Clemson, Alabama, Ohio State, Florida State'
+print(prev_ten_cfb_champs)
+world_cup_champ = 'Argentina'
+print(world_cup_champ)
+sayings_one = 'No dar papaya'
+print(sayings_one)
+sayings_two = 'In for a penny, in for a pound'
+print(sayings_two)
+sayings_three = 'You cannot tell a hungry child that you gave them food yesterday'
+print(sayings_three)
+wisdom = 'Every good and perfect gift comes from above, and cometh down from the Father of lights, with whom is no variableness, or shadow of turning.'
+print(wisdom)
+which_way = 'to the right'
+print(which_way)
+processor = 'intel core i7'
+print(processor)
+monitor = 'dual screen'
+print(monitor)
+squat_pr = '315lbs'
+print(squat_pr)
+when_you_wake = 'set goals for the day'
+print(when_you_wake)
+
+# 20 lines of comments
+
+# works for me
+# control + / is the shortcut for commenting a line 
+# the home and end keys are good for maneuvering on a line
+# this is awesome
+# bit by bit I'm learning more of the syntax involved with python
+# alt + up or down moves a line of code in that subsequent direction
+# the openness of the language lends itself to creative minds
+# I wonder, if one isn't accustomed to creativity how can it be learned?
+# By practice of course
+# taking time to critically think
+# your motivations
+# your intentions
+# your influences
+# and ultimately your end goal
+# just a man and his thoughts
+# then a man of action
+# Will you also teach us of the ''' ''' triple single quotes comments?
+# What about str literals, str variables, or passing str through a function?
+# Perhaps those are best learned later or during self-study
+# The possibilities are endless
