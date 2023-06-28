cd66645994a1e306422462c4b9b9436352277616
ldatareserve <lnowlin@mail.com>
2023-06-28 15:32:23 -0400

    Week 3 of IP Python class

---
+++ week3.py
@@ -0,0 +1,79 @@
+# storing numbers in str
+a_num = 12
+print(a_num)
+b_num = 24
+print(b_num)
+c_num = 36
+print(c_num)
+d_num = 48
+print(d_num)
+e_num = 60
+print(e_num)
+
+# converting num to str w str()
+age = 30
+print(str(age) + ' is a good age to get your passport.')
+three_digits = 100
+print(str(three_digits) + ' has three digits.')
+weight = 1
+print(str(weight) + 'ton equals 1000 kgs.') 
+agent_fees = 7.8
+print('Lamar Jackson saved $' + str(agent_fees) + 'M negotiating alone.')
+wr = 9.58
+print(str(wr) + 's is the fastest recorded 100m in the world.')
+
+# absolute val func
+my_num = -30
+print(abs(my_num))
+my_num2 = 7
+print(abs(my_num2))
+my_num3 = 17
+print(abs(my_num3))
+my_num4 = -304
+print(abs(my_num4))
+my_num5 = 777
+print(abs(my_num5))
+
+# power func
+print(pow(1,2))
+print(pow(222,46))
+print(pow(7,3))
+print(pow(80,9))
+print(pow(10,5))
+
+# Max func
+print(max(3,4))
+print(max(11,10))
+print(max(9,72))
+print(max(2,1))
+print(max(8,0))
+
+#Min func
+print(min(0,1))
+print(min(4,2,))
+print(min(10000,100000))
+print(min(45,42))
+print(min(7,8))
+
+# Round func
+print(round(8.5))
+print(round(7.3))
+print(round(9.9))
+print(round(4.1))
+print(round(2.8))
+
+# import func (from math library)
+from math import *
+print(sqrt(100))
+print(sqrt(49))
+print(sqrt(9))
+print(sqrt(144))
+print(sqrt(25))
+
+from math import *
+print(tan(1))
+print(tan(15))
+print(tan(7))
+print(tan(900))
+print(tan(pi/6))
+
