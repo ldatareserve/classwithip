f6cead03357e2e035249fc1a6131aeb3ac1a034b
ldatareserve <lnowlin@mail.com>
2023-06-29 18:06:44 -0400

    Week 5 of IP Python class

---
+++ week5.py
@@ -0,0 +1,78 @@
+# Lists
+digits = [1,2,3,4,5,6,7]
+print(digits)
+
+numbers = ['one','two','three','five','six','seven']
+print(numbers)
+
+oceans = ['Atlantic','Pacific','Indian','Southern','Arctic']
+print(oceans)
+
+directions = ['North','South','East','West']
+print(directions)
+
+top_leagues = ['Premier League','Bundesliga','La Liga','Serie A']
+print(top_leagues)
+
+
+# Printing one element from list
+noble_gases = ['Argon','Radon','Neon','Krypton']
+print(noble_gases[1])
+
+bones = ['Femur','Clavicle','Tibia','Occipital']
+print(bones[3])
+
+hydrocarbons = ['Methane','Ethane','Propane','Butane','Pentane','Hexane','Heptane',]
+print(hydrocarbons[6])
+
+mlb = ['Orioles','Cubs','Red Sox','Yankees']
+print(mlb[0])
+
+grocers = ['Walmart','Costco','Meijer','Publix','Food Lion']
+print(grocers[2])
+
+
+# Printing multilple elements from a list
+us_rivers = ['Mississippi','Ohio','Rio Grande','Missouri','Colorado']
+print(us_rivers[0:3])
+
+transformers = ['Optimus','Bumblebee','Megatron','Starscream']
+print(transformers[1:3])
+
+tools = ['Wrench','Hammer','Shovel','Pliers','Saw']
+print(tools[2:5])
+
+colors = ['Red','Blue','Yellow','Gold','Silver','Crystal']
+print(colors[0:5])
+
+languages = ['Spanish','Japanese','Portuguese','Tagalog','Zulu']
+print(languages[3:5])
+
+
+# Replacing one element for another in list
+nfl = ['Colts','Patriots','Cowboys','49ers','76ers']
+nfl[4] = 'Eagles'
+print(nfl)
+print(nfl[4])
+
+nba = ['Bulls','Knicks','Dolphins','Lakers','Celtics']
+nba[2] = 'Heat'
+print(nba)
+print(nba[2])
+
+continents = ['N.America','S.America','Europe','Africa','Australia','Asia','Arctica']
+continents[6] = 'Antarctica'
+print(continents)
+print(continents[6])
+
+fish = ['Catfish','Aligator','Carp','Tuna','Salmon']
+fish[1] = 'Marlin'
+print(fish)
+print(fish[1])
+
+nitrogenous_bases = ['Amine','Thymine','Cytosine','Guanine','Uracil']
+nitrogenous_bases[0] = 'Adenine'
+print(nitrogenous_bases)
+print(nitrogenous_bases[0])
+
+
