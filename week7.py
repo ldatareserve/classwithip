23c9d292c8e9fafc3040c26a42b6857b9addbd56
ldatareserve <lnowlin@mail.com>
2023-06-30 15:06:53 -0400

    Week 7 of IP Python class

---
+++ week7.py
@@ -0,0 +1,40 @@
+# Tuples
+water = ('Fiji','Ice Moutain','Core','Essentia')
+print(water)
+
+sodas = ('Coke','Sprite','Pepsi','Dr. Pepper')
+print(sodas)
+
+
+# Printing single element from tuple
+numbers = (7,3,9,1,3,2)
+print(numbers[4])
+
+oly_sprints = ('100m','200m','400m','4x100m')
+print(oly_sprints[1])
+
+
+# Tuples immutable characteristics
+writing_utensils = ('Pen','Pencil','Brush','Marker')
+writing_utensils[1] = 'Crayon'
+print(writing_utensils[1])
+
+streaming_service = ('ESPN+','NFL+','Peacock','Hulu')
+streaming_service[0] = 'YoutubeTV'
+print(streaming_service[0])
+
+rocks = ('igneous','sedimentary','metamorphic')
+rocks[1] = 'limestone'
+print(rocks[1])
+
+digits = (9,8,0,7,4,3,48,1)
+digits[6] = '100'
+print(digits[6])
+
+phone_service = ('T-Mobile','Verizon','Boost','AT&T','Metro')
+phone_service[4] = 'Mint'
+print(phone_service[4])
+
+rappers = ('Tupac','Biggie','Dr. Dre','Nas')
+rappers[3] = 'Andre 3000'
+print(rappers[3])
